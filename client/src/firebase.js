1fde3c14ff8bb66ee8b7e6a5357f52161a7bdb62
Vivek Korah <lcb2021051@iiitl.ac.in>
2025-04-07 22:44:10 +0530

    api key name fix

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,18 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-require("dotenv").config();
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API,
+  apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: "placement-point-33d88.firebaseapp.com",
   projectId: "placement-point-33d88",
-  storageBucket: "placement-point-33d88.firebasestorage.app",
+  storageBucket: "placement-point-33d88.appspot.com",
   messagingSenderId: "309398785222",
   appId: "1:309398785222:web:94b0986c22a6045a341bc7",
   measurementId: "G-LVX06SK2TZ",
 };
 
-// Initialize Firebase
+console.log(process.env.REACT_APP_FIREBASE_API);
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
