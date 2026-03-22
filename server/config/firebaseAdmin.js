2975b371d9ca0478abffd70c05fffb0a0a848578
Vivek Korah <lcb2021051@iiitl.ac.in>
2026-03-22 09:23:16 +0530

    service account path update

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -1,5 +1,15 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const fs = require("fs");
+const path = require("path");
+
+const possiblePaths = [
+  path.join(__dirname, "serviceAccountKey.json"),
+  "/etc/secrets/serviceAccountKey.json",
+];
+
+const filePath = possiblePaths.find((p) => fs.existsSync(p));
+if (!filePath) throw new Error("serviceAccountKey.json not found");
+const serviceAccount = JSON.parse(fs.readFileSync(filePath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
